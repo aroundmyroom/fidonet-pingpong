6f7f77ad9481fac23ac0565c77d71bd236c3fdeb
AroundMyRoom <dennis@aroundmyroom.com>
2024-11-03 20:17:06 +0100

    Update ping.pl
    added changes for the logging based on feedback of Jay Harris, also added date/time and logging to logfile as the original log does not show the info due to tmux

--- ping.pl
+++ ping.pl
@@ -1,82 +1,71 @@
-#!/usr/bin/perl
-#
-# Ping-pong robot for HPT. Designed accordingly FTS-5001.002
-# (c) 2006 Gremlin
-# (c) 2006 Grumbler
-# (c) 2010 Grumbler
-#
-# Modified by Jay Harris (1:229/664) with thanks to
-# Deon George (3:633/509).
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# Insert into config:
-# hptperlfile /home/fido/lib/filter.pl
-# and place to filter.pl some like this:
-# BEGIN { require "pong.pl"; }
-# sub filter{
-#   &pong;
-# }
-#
+use POSIX qw(strftime);
 
 sub w_log {
     my ($type, $message) = @_;
-    print "[$type] $message\n";  # Adjust as needed
+    my $logfile = '/var/log/husky/ping.log';  # Path to the log file
+
+    # Check if the directory exists; create it if it doesn’t
+    my $logdir = '/var/log/husky';
+    unless (-d $logdir) {
+        mkdir $logdir or die "Could not create directory '$logdir': $!";
+    }
+
+    # Format the current date and time
+    my $timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime);
+
+    # Open the log file in append mode; create it if it doesn’t exist
+    open my $fh, '>>', $logfile or die "Could not open log file '$logfile': $!";
+    
+    # Add timestamp to the log message
+    print $fh "[$timestamp] [$type] $message\n";
+    close $fh;
 }
 
-
+my ($file)   = __FILE__ =~ /([^\/]+)$/; # Get filename of script without path
 my $flagfile = '/var/spool/ftn/flags/netscan'; # Flag file for indicating new netmail
 my $myname   = 'AroundMyRooms Ping Robot'; # From: name in PONG reply
 my $origline = 'You sent a ping! That did hurt, I will tell mamma!'; # Origin Line
 my @myaddr   = @{ $config{addr} };
 my $myaddr   = $myaddr[0];
 
-sub pong() {
+sub pong {
     if ( grep { $_ eq $toaddr } @myaddr ) {
-        #Respond from the address ping was sent to
+        # Respond from the address ping was sent to
         $myaddr = $toaddr;
         $pngtr = "Your PING request has been received at its final destination:";
-        $pngsub = "PONG:";
+        $pngsub = "PONG";
     }
     else {
         $pngtr  = "Your in transit PING was received and routed onward:";
-        $pngsub = "TRACE:";
+        $pngsub = "TRACE";
 
-        #Get zone of sender
+        # Get zone of sender
         ($fromzone) = $fromaddr =~ /^(.*?)(?=:)/;
-        }
-        #If othernet, match sender's zone with an address on this system
-        if ( $fromzone !~ /^[1234]$/ ) {
-            foreach (@myaddr) {
-                ($myzone) = $_ =~ /^(.*?)(?=:)/;
-                if ( $myzone == $fromzone ) {
-                    $myaddr = $_;
-                    last;
+    }
+
+    # If othernet, match sender's zone with an address on this system
+    if ( $fromzone !~ /^[1234]$/ ) {
+        foreach (@myaddr) {
+            ($myzone) = $_ =~ /^(.*?)(?=:)/;
+            if ( $myzone == $fromzone ) {
+                $myaddr = $_;
+                last;
             }
         }
     }
 
     my $msgtext = "";
 
-  # Check if message is netmail & addressed to PING (case insensitive)
-     if ((length($area)==0) && (uc $toname eq "PING") && (uc $fromname ne "PING")) {
+    # Check if message is netmail & addressed to PING (case insensitive)
+    if ((length($area)==0) && (uc $toname eq "PING") && (uc $fromname ne "PING")) {
 
-        w_log('C',"Perl(): Make PONG to PING request: area=".((length($area)==0)? "netmail":$area)."; toname=$toname; toaddr=$toaddr fromname=$fromname; fromaddr=$fromaddr" );
+        w_log('C', "$file: Make $pngsub to PING request: area=".((length($area)==0)? "netmail":$area)."; toname=$toname; toaddr=$toaddr fromname=$fromname; fromaddr=$fromaddr" );
 
         # Kill ping netmails addressed to this system
         if ( grep { $_ eq $toaddr } @myaddr ) {
-        #if you want to keep the original message before the netmail is killed, add this line, make sure the LOCAL area exist
-        putMsgInArea('PING', $fromname, $toname, $fromaddr, $toaddr, $subject, $date, $attr, $text, 0);
-        # below setting will set the kill to the message
-
+            # If you want to keep the original message before the netmail is killed, add this line
+            putMsgInArea('PING', $fromname, $toname, $fromaddr, $toaddr, $subject, $date, $attr, $text, 0);
+            # Below setting will set the kill to the message
             $kill = 1;
         }
 
@@ -87,7 +76,7 @@ sub pong() {
         $msgtext = $text;
 
         # Get MSGID (if any) for REPLY: kludge
-         if ( $msgtext =~ /\r\x01MSGID:\s*(.*?)\r/ ) {
+        if ( $msgtext =~ /\r\x01MSGID:\s*(.*?)\r/ ) {
             $RPLY = $1;
         }
 
@@ -125,8 +114,8 @@ sub pong() {
         }
 
         # Post message
-        my $err = putMsgInArea($area,$myname,$fromname,$myaddr,$fromaddr,"$pngsub ".$subject,"","Uns Loc Pvt K/s",$msgtext,3);
-        if( defined($err) ){ w_log('A',"Perl(): Can't make new message: $err"); }
+        my $err = putMsgInArea($area,$myname,$fromname,$myaddr,$fromaddr,"$pngsub: ".$subject,"","Uns Loc Pvt K/s",$msgtext,3);
+        if( defined($err) ){ w_log('A',"$file: Unable to make a PONG reply: $err"); }
         else{ open( FLAG, ">>$flagfile" ) && close(FLAG); }
     }
     return "";
